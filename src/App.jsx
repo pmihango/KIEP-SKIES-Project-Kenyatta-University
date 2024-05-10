90400a8065062d097df3959011f0814b4c5706ea
pmihango <pmihango@gmail.com>
2024-05-10 14:52:25 +0300

    added the sections title and subtitles as props on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Services from './components/Services/Services'
+import Title from './components/title/Title'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
           <Navbar/>
           <Hero/>
         <div className="contaner">
+          <Title subTitle='Our Services' title='What We Offer'/>
           <Services/>
         </div>
       
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Title.css'
+
+const Title = ({SubTitle, title}) => {
+  return (
+    <div className='title'> 
+    <p>{SubTitle}</p>
+    <h2>{title}</h2>
+
+    </div>
+  )
+}
+
+export default Title
