0b4b4088b09feba06ab166c88a733503b90a732c
pmihango <pmihango@gmail.com>
2024-05-10 09:02:15 +0300

    added the logo and Navebar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/urex.png'
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+        <img src={logo} alt="" className='logo' />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact Us</button></li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar
