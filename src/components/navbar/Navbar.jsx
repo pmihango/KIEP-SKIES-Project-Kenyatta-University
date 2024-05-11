7863ec6b468e27cfe7335854dee08f01baead024
pmihango <pmihango@gmail.com>
2024-05-11 17:21:14 +0300

    added the smooth scroll fuctionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/urex.png'
+import { Link, ScrollLink } from 'react-scroll';
 
 const Navbar = () => {
    const [sticky, setSticky] = useState(false);
@@ -13,12 +14,12 @@ const Navbar = () => {
     <nav className={`container ${sticky? 'dark-nav' : ''}`} >
         <img src={logo} alt="" className='logo' />
         <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>About Us</li>
-            <li>Projects</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact Us</button></li>
+            <li> <Link to='hero' smooth={true} offset={0} duration={500}>Home</Link> </li>
+            <li> <Link to='services' smooth={true} offset={-100} duration={500}>Services</Link></li>
+            <li> <Link to='about' smooth={true} offset={0} duration={500}>About Us</Link></li>
+            <li> <Link to='projects' smooth={true} offset={-100} duration={500}>Projects</Link></li>
+            <li> <Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
+            <li> <Link to='contact' smooth={true} offset={0} duration={500}>ContactUs</Link></li>
         </ul>
 
     </nav>
