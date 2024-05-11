8a04c616ef1b2f551aa63fa3df765e1f78d49ac5
pmihango <pmihango@gmail.com>
2024-05-11 12:03:51 +0300

    added the contact form and footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,6 +8,32 @@ import location_icon from '../../assets/location.png'
 
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "6b299d41-acad-4605-bc7f-f21068243116");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Message Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className='contact'>
         <div className="contact-col">
@@ -23,10 +49,19 @@ const Contact = () => {
                 <li><img src={web_icon} alt="" />https://urexgroup.co.ke</li>
                 <li><img src={mail_icon} alt="" />pmihango@gmail.com</li>
                 <li><img src={phone_icon} alt="" />+254 720 078 029 </li>
-                <li><img src={location_icon} alt="" />Chandaria Business Center, Thika Road, K.U Main Gate.</li>
+                <li><img src={location_icon} alt="" />Chandaria Business Center, <br/>Thika Road, K.U Main Gate.</li>
             </ul>
         </div>
-        <div className="contact-col"></div>
+        <div className="contact-col">
+          <form onSubmit={onSubmit}>
+            <label>Your Name</label> <input type="text" name='name' placeholder='enter your name' required/>
+            <label>Phone Number</label> <input type="tel" name='phone' placeholder='enter your mobile number' required/>
+            <label>Write Your Message Here</label> <textarea name="message" rows="6" placeholder='enter your message here' required></textarea>
+            <button type='submit' className='btn dark-btn'>Send Now</button>
+
+          </form>
+          <span>{result}</span>
+        </div>
 
     </div>
   )
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className="footer">
+            <p>Copyright © 2024 Paul Mihango</p>
+            <ul>
+                <li>Terms of Service</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
